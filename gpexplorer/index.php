7f33b4306a39c991739f28a00f715dba41f573d1
Jeremy FRAZIER <frazierjeremi@gmail.com>
2023-09-27 09:49:15 +0200

    index.php

---
+++ gpexplorer/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gp Explorer</title>
+</head>
+<body>
+    <header>
+        <h1></h1>
+        <nav>
+            <ul>
+                <li>Accueil</li>
+                <li>Temps Réel</li>
+                <li>High Score</li>
+            </ul>
+        </nav>
+    </header>
+    <section id="presentation">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, fugiat 
+    consequatur eos id magni minima dolores dicta dolorum quis officiis enim voluptas 
+    repellat sint nostrum laudantium laborum reprehenderit unde. Molestiae?</p>
+    </section>
+    <section id="highscore">
+
+    </section>
+    
+    
+    <footer>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </footer>
+
+</body>
+</html>
