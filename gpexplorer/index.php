a29baa4e4984c82b80da5f7ee5d6389f324e45f1
Jeremy FRAZIER <frazierjeremi@gmail.com>
2023-09-27 10:20:15 +0200

    index.php

--- gpexplorer/index.php
+++ gpexplorer/index.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gp Explorer</title>
+    <link rel="stylesheet" href="assets/styles.css">
 </head>
 <body>
     <header>
         <h1></h1>
         <nav>
             <ul>
-                <li>Accueil</li>
-                <li>Temps Réel</li>
+                <li>Accueil </li> 
+                <li>Temps Réel </li> 
                 <li>High Score</li>
             </ul>
         </nav>
@@ -22,10 +23,18 @@
     repellat sint nostrum laudantium laborum reprehenderit unde. Molestiae?</p>
     </section>
     <section id="highscore">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, molestias iure aliquam excepturi inventore illum dolor accusamus animi aperiam dolorem praesentium voluptatibus! Aliquid deleniti, ducimus magni nulla qui sint ex! : 
+        <!-- 
+
+            Dans cette zone tous les nouveaux scores
+
+        -->
 
     </section>
     <section id="rules">
-
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. At corrupti voluptate repellendus esse, accusamus ipsam placeat soluta nostrum quisquam hic ullam inventore nemo doloremque incidunt modi laboriosam fugiat! Unde, commodi.
+    Nostrum, veniam! Nisi quis autem alias rem accusantium repudiandae eos inventore voluptas quia dolor. Mollitia ducimus exercitationem quidem. Repudiandae accusamus maxime illo? Corporis accusantium iusto ullam adipisci dignissimos, impedit error.
+    Eligendi nulla ab, velit dolorum quas porro dicta quasi eum, facere deserunt vero consequuntur! Harum, corporis repellat quos perferendis officia velit, id ipsum necessitatibus neque beatae pariatur adipisci ducimus. Earum.
     </section>
     
     <footer>
